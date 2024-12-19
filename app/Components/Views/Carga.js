b12b564e2412f830479c59b03df6b2cefa200c35
Beracoechea <jose.beracoechea5098@alumnos.udg.mx>
2024-12-18 21:14:30 -0600

    Pantalla de carga

--- app/Components/Views/Carga.js
+++ app/Components/Views/Carga.js
@@ -1,40 +1,68 @@
-import {  View, StyleSheet, ImageBackground,ActivityIndicator,} from 'react-native';
 import React, { useEffect } from 'react';
-import {useRouter } from 'expo-router';
-import splash from '../../../assets/splash.png'
-import { Colores } from '../Generic/Colors';
-
-
+import { View, StyleSheet, Text } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { useRouter } from 'expo-router';
+import Colors from '../Generic/Colors';
 
 const Carga = () => {
   const router = useRouter();
 
-  useEffect(() => {
+   useEffect(() => {
     const timeout = setTimeout(() => {
       router.replace('/SeleccionInicio');
     }, 3500);
 
-    return () => clearTimeout(timeout); 
+    return () => clearTimeout(timeout);
   }, [router]);
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={splash} resizeMode="cover" style={styles.image}>
-        <ActivityIndicator size="large" color={Colores.OliveHarves} />
-      </ImageBackground>
+      <View style={styles.textContainer}>
+        {['V', 'e', 't', 't', 'e', 'r'].map((letter, index) => (
+          <Text key={index} style={[styles.text, { color: colors[index % colors.length] }]}>
+            {letter}
+          </Text>
+        ))}
+      </View>
+      <LottieView
+        source={require('../../../assets/carga.json')}
+        autoPlay
+        loop
+        style={styles.animation}
+        speed={1}
+      />
+      <View style={styles.textContainer}>
+        {['D', 'o', 'g'].map((letter, index) => (
+          <Text key={index} style={[styles.text, { color: colors[index % colors.length] }]}>
+            {letter}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 };
+
+const colors = [Colors.GoldennBatter, Colors.ToastedCaramel, Colors.SpicedWine, Colors.OliveHarves, Colors.CowhineCocoa];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
-  image: {
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
+  textContainer: {
+    flexDirection: 'row',
+    marginBottom: 20, 
+  },
+  text: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginHorizontal: 2,
+  },
+  animation: {
+    width: 200,
+    height: 200,
   },
 });
 
